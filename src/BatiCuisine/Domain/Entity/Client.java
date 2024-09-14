38d7a7299bb3773c281c4563025df53bc3873e1f
Khalid Zennouhi <khalidzennouhi08@gmail.com>
2024-09-15 00:31:12 +0200

    create the class project with there getter & setter, there constructur and the toString method

--- src/BatiCuisine/Domain/Entity/Client.java
+++ src/BatiCuisine/Domain/Entity/Client.java
@@ -1,4 +1,63 @@
 package BatiCuisine.Domain.Entity;
 
 public class Client {
+    private Double id;
+    private String name;
+    private String address;
+    private String phone;
+    private Boolean isProfessional;
+
+    public Client() {
+    }
+
+    public Double getId() {
+        return id;
+    }
+
+    public void setId(Double id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Boolean getProfessional() {
+        return isProfessional;
+    }
+
+    public void setProfessional(Boolean professional) {
+        isProfessional = professional;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                ", isProfessional=" + isProfessional +
+                '}';
+    }
 }
