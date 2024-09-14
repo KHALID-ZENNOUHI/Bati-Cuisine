04e61e7aa2f49e117a1a6d9a4a8a5117787f6407
Khalid Zennouhi <khalidzennouhi08@gmail.com>
2024-09-15 00:41:07 +0200

    create the Labor class that enhirete from the Component class with there getter & setter, the constructur, the toString method and the method of calculateCost

--- src/BatiCuisine/Domain/Entity/Labor.java
+++ src/BatiCuisine/Domain/Entity/Labor.java
@@ -1,4 +1,55 @@
 package BatiCuisine.Domain.Entity;
 
-public class Labor {
+public class Labor extends Component{
+    private Double hourlyRate;
+    private Double hoursWorked;
+    private  Double workerProductivity;
+
+    public Labor(Double id, String name, Double unitCost, Double quantity, String componentType, Double VATRate, Double hourlyRate, Double hoursWorked, Double workerProductivity) {
+        super(id, name, unitCost, quantity, componentType, VATRate);
+        this.hourlyRate = hourlyRate;
+        this.hoursWorked = hoursWorked;
+        this.workerProductivity = workerProductivity;
+    }
+
+    public Labor() {
+        super();
+    }
+
+    public Double getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(Double hourlyRate) {
+        this.hourlyRate = hourlyRate;
+    }
+
+    public Double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(Double hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public Double getWorkerProductivity() {
+        return workerProductivity;
+    }
+
+    public void setWorkerProductivity(Double workerProductivity) {
+        this.workerProductivity = workerProductivity;
+    }
+
+    public Double calculateCost() {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "Labor{" +
+                "hourlyRate=" + hourlyRate +
+                ", hoursWorked=" + hoursWorked +
+                ", workerProductivity=" + workerProductivity +
+                '}';
+    }
 }
