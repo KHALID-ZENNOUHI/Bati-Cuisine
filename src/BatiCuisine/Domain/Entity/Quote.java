a94de170c3ba4b304afd8baaa113611a5c5a5abe
Khalid Zennouhi <khalidzennouhi08@gmail.com>
2024-09-15 00:35:06 +0200

    create the class project with there getter & setter, there constructur and the toString method

--- src/BatiCuisine/Domain/Entity/Quote.java
+++ src/BatiCuisine/Domain/Entity/Quote.java
@@ -1,4 +1,73 @@
 package BatiCuisine.Domain.Entity;
 
+import java.time.LocalDate;
+
 public class Quote {
+    private Double id;
+    private Double estimatedAmount;
+    private LocalDate issueDate;
+    private LocalDate validityDate;
+    private Boolean accepted;
+
+    public Quote(Double id, Double estimatedAmount, LocalDate issueDate, LocalDate validityDate, Boolean accepted) {
+        this.id = id;
+        this.estimatedAmount = estimatedAmount;
+        this.issueDate = issueDate;
+        this.validityDate = validityDate;
+        this.accepted = accepted;
+    }
+
+    public Quote() {
+    }
+
+    public Double getId() {
+        return id;
+    }
+
+    public void setId(Double id) {
+        this.id = id;
+    }
+
+    public Double getEstimatedAmount() {
+        return estimatedAmount;
+    }
+
+    public void setEstimatedAmount(Double estimatedAmount) {
+        this.estimatedAmount = estimatedAmount;
+    }
+
+    public LocalDate getIssueDate() {
+        return issueDate;
+    }
+
+    public void setIssueDate(LocalDate issueDate) {
+        this.issueDate = issueDate;
+    }
+
+    public LocalDate getValidityDate() {
+        return validityDate;
+    }
+
+    public void setValidityDate(LocalDate validityDate) {
+        this.validityDate = validityDate;
+    }
+
+    public Boolean getAccepted() {
+        return accepted;
+    }
+
+    public void setAccepted(Boolean accepted) {
+        this.accepted = accepted;
+    }
+
+    @Override
+    public String toString() {
+        return "Quote{" +
+                "id=" + id +
+                ", estimatedAmount=" + estimatedAmount +
+                ", issueDate=" + issueDate +
+                ", validityDate=" + validityDate +
+                ", accepted=" + accepted +
+                '}';
+    }
 }
