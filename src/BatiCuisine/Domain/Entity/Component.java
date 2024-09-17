c11c967637ef960ac0f16a91fd398b0b1368ef96
Khalid Zennouhi <khalidzennouhi08@gmail.com>
2024-09-17 12:44:33 +0200

    delete the unitCost and the quantity from the component and change the type of the componentType to the ComponentType enum

--- src/BatiCuisine/Domain/Entity/Component.java
+++ src/BatiCuisine/Domain/Entity/Component.java
@@ -1,19 +1,18 @@
 package BatiCuisine.Domain.Entity;
 
+import BatiCuisine.Domain.Enum.ComponentType;
+
 public abstract class Component {
     private int id;
     private String name;
-    private Double unitCost;
-    private Double quantity;
-    private String componentType;
+    private ComponentType componentType;
     private Double VATRate;
     private int projectId;
 
-    public Component(int id, String name, Double unitCost, Double quantity, String componentType, Double VATRate, int projectId) {
+
+    public Component(int id, String name, ComponentType componentType, Double VATRate, int projectId) {
         this.id = id;
         this.name = name;
-        this.unitCost = unitCost;
-        this.quantity = quantity;
         this.componentType = componentType;
         this.VATRate = VATRate;
         this.projectId = projectId;
@@ -40,27 +39,11 @@ public abstract class Component {
         this.name = name;
     }
 
-    public Double getUnitCost() {
-        return unitCost;
-    }
-
-    public void setUnitCost(Double unitCost) {
-        this.unitCost = unitCost;
-    }
-
-    public Double getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Double quantity) {
-        this.quantity = quantity;
-    }
-
-    public String getComponentType() {
+    public ComponentType getComponentType() {
         return componentType;
     }
 
-    public void setComponentType(String componentType) {
+    public void setComponentType(ComponentType componentType) {
         this.componentType = componentType;
     }
 
@@ -87,10 +70,9 @@ public abstract class Component {
         return "Component{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
-                ", unitCost=" + unitCost +
-                ", quantity=" + quantity +
                 ", componentType='" + componentType + '\'' +
                 ", VATRate=" + VATRate +
+                ", projectId=" + projectId +
                 '}';
     }
 }
