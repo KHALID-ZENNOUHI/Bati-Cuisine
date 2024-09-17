33f6e3f6fe348c8e3998a7328791b285c0d6b5b8
Khalid Zennouhi <khalidzennouhi08@gmail.com>
2024-09-17 15:50:03 +0200

    add the Component interface that have the methods that i use in the ComponentImplementation

--- src/BatiCuisine/Repository/Interface/ComponentInterface.java
+++ src/BatiCuisine/Repository/Interface/ComponentInterface.java
@@ -1,4 +1,20 @@
 package BatiCuisine.Repository.Interface;
 
+import BatiCuisine.Domain.Entity.Component;
+import BatiCuisine.Domain.Entity.Labor;
+import BatiCuisine.Domain.Entity.Material;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface ComponentInterface {
+    public int save(Component component);
+    public Labor saveLabor(Labor labor);
+    public Material saveMaterial(Material material);
+    public int update(Component component);
+    public Labor updateLabor(Labor labor);
+    public Material updateMaterial(Material material);
+    public Boolean delete(int id);
+    public Optional<Component> findById(int id);
+    public List<Component> findAll();
 }
