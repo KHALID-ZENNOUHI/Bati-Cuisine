c675c6b06203c300be2ea234f0053b1a08a7b41e
Khalid Zennouhi <khalidzennouhi08@gmail.com>
2024-09-17 12:51:44 +0200

    for adapting the repository desing pattern i create the clientInterface that have all the methods i need for the client

--- src/BatiCuisine/Repository/Interface/ClientInterface.java
+++ src/BatiCuisine/Repository/Interface/ClientInterface.java
@@ -1,13 +1,17 @@
 package BatiCuisine.Repository.Interface;
 
+import BatiCuisine.Domain.Entity.Client;
+import BatiCuisine.Domain.Entity.Project;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
-public interface InterfaceDAO {
-    public  Object save(HashMap<String, Object> data);
-    public  Object update(HashMap<String, Object> data);
-    public Optional<Object> delete(int id);
-    public Optional<Object> findById(int id);
-    public List<Object> findAll();
+public interface ClientInterface {
+    public Client save(Client client);
+    public Client update(Client client);
+    public Boolean delete(int id);
+    public Optional<Client> findByName(String name);
+    public List<Client> findAll();
+    public List<Project> getProjects(int id);
 }
