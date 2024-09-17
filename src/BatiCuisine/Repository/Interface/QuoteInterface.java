5c07d9507282ee1c2fc001fd002ba58562f43330
Khalid Zennouhi <khalidzennouhi08@gmail.com>
2024-09-17 17:03:43 +0200

    add the Quote interface to indacacte the methods that i'll use and the  implementation of quote that implement those methods to interact with the database

--- src/BatiCuisine/Repository/Interface/QuoteInterface.java
+++ src/BatiCuisine/Repository/Interface/QuoteInterface.java
@@ -1,4 +1,15 @@
 package BatiCuisine.Repository.Interface;
 
+import BatiCuisine.Domain.Entity.Quote;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface QuoteInterface {
+    public Quote save(Quote quote);
+    public Quote update(Quote quote);
+    public Boolean delete(int id);
+    public Optional<Quote> findById(int id);
+    public Optional<Quote> findByProjectId(int id);
+    public List<Quote> findAll();
 }
