e52ac2c77465ee330d1004cb44ebdf9f28fbce0a
Khalid Zennouhi <khalidzennouhi08@gmail.com>
2024-09-17 12:47:00 +0200

    change the type of the componenttype in the constructur of labor

--- src/BatiCuisine/Domain/Entity/Labor.java
+++ src/BatiCuisine/Domain/Entity/Labor.java
@@ -1,12 +1,14 @@
 package BatiCuisine.Domain.Entity;
 
+import BatiCuisine.Domain.Enum.ComponentType;
+
 public class Labor extends Component{
     private Double hourlyRate;
     private Double hoursWorked;
     private  Double workerProductivity;
 
-    public Labor(int id, String name, Double unitCost, Double quantity, String componentType, Double VATRate, int projectId, Double hourlyRate, Double hoursWorked, Double workerProductivity) {
-        super(id, name, unitCost, quantity, componentType, VATRate, projectId);
+    public Labor(int id, String name, ComponentType componentType, Double VATRate, int projectId, Double hourlyRate, Double hoursWorked, Double workerProductivity) {
+        super(id, name, componentType, VATRate, projectId);
         this.hourlyRate = hourlyRate;
         this.hoursWorked = hoursWorked;
         this.workerProductivity = workerProductivity;
