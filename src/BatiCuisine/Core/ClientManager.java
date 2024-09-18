6fb714aa76983987e7fcd39ccaac75b7a020fe16
Khalid Zennouhi <khalidzennouhi08@gmail.com>
2024-09-18 18:20:28 +0200

    add the manager of client to let user create, update, delete, and find all projects of client

--- src/BatiCuisine/Core/ClientManager.java
+++ src/BatiCuisine/Core/ClientManager.java
@@ -1,4 +1,110 @@
 package BatiCuisine.Core;
 
+import BatiCuisine.Domain.Entity.Client;
+import BatiCuisine.Domain.Entity.Project;
+import BatiCuisine.Service.Implementation.ClientServiceImpl;
+
+import java.sql.SQLException;
+import java.sql.SQLOutput;
+import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
+
 public class ClientManager {
+    private ClientServiceImpl clientService;
+    private Scanner scanner;
+
+    public ClientManager() throws SQLException {
+        this.clientService = new ClientServiceImpl();
+        this.scanner = new Scanner(System.in);
+    }
+
+    public int createClient() {
+        System.out.print("Enter client name: ");
+        String name = scanner.nextLine();
+        System.out.print("Enter client address: ");
+        String address = scanner.nextLine();
+        System.out.print("Enter client phone number: ");
+        String phoneNumber = scanner.nextLine();
+        String isPro;
+        do {
+           System.out.print("Is the client a professional? (y/n)");
+           isPro = scanner.nextLine();
+       }while (!isPro.equals("y") && !isPro.equals("n"));
+        Boolean isProfessional = isPro.equals("y");
+        Client client = new Client(name, address, phoneNumber, isProfessional);
+        Client savedClient = clientService.save(client);
+        if (savedClient == null) {
+            System.out.println("Client created successfully with name: " + savedClient.getName());
+            return savedClient.getId();
+        } else {
+            System.out.println("Error creating client.");
+            return -1;
+        }
+
+    }
+
+    public int searchClient() {
+        System.out.print("Enter client name: ");
+        String name = scanner.nextLine();
+        Client client = clientService.findByName(name).orElse(null);
+        if (client != null) {
+            System.out.println("Client found!\n" +
+                    "Name: " + client.getName() + "\n" +
+                    "Address: " + client.getAddress() + "\n" +
+                    "Phone: " + client.getPhone());
+            return client.getId();
+        } else {
+            System.out.println("Client not found.");
+            return -1;
+        }
+    }
+
+    public void updateClient () {
+        System.out.print("Enter client name you want to update there info: ");
+        String name = scanner.nextLine();
+        Client client = clientService.findByName(name).orElse(null);
+        if (client != null) {
+            System.out.print("Enter the new name of client: ");
+            String newName =  scanner.nextLine();
+            System.out.print("Enter the new client address: ");
+            String address = scanner.nextLine();
+            System.out.print("Enter the new client phone number: ");
+            String phoneNumber = scanner.nextLine();
+            String isPro;
+            do {
+                System.out.print("Is the client a professional? (y/n)");
+                isPro = scanner.nextLine();
+            }while (!isPro.equals("y") && !isPro.equals("n"));
+            Boolean isProfessional = isPro.equals("y");
+            Client newClient = new Client(name, address, phoneNumber, isProfessional);
+            clientService.update(newClient);
+            System.out.println("Client created successfully!");
+        }
+    }
+
+    public void deleteClient () {
+        System.out.print("Enter the name of the client you want to delete : ");
+        String name = scanner.nextLine();
+        Optional<Client> clientOptional = clientService.findByName(name);
+        if (clientOptional.isPresent()) {
+            Client client = clientOptional.get();
+            int id =  client.getId();
+            Boolean isDeleted = clientService.delete(id);
+            if (isDeleted) System.out.println("The client with the name" + name + "is deleted successfully!");
+            else System.out.println("An error occurred on the delete of the client! ");
+        } else {
+            System.out.println("There is no client with the name: " + name);
+        }
+    }
+
+    public void getClientProjects() {
+        System.out.println("Enter the name of the client you want there project: ");
+        String name  = scanner.nextLine();
+        Optional<Client> clientOptional = clientService.findByName(name);
+        if (clientOptional.isPresent()){
+            Client  client = clientOptional.get();
+            clientService.clientProjects(client.getId()).forEach(System.out::println);
+        }
+    }
 }
