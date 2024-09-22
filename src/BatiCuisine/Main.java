9bc36cc1b18e82011564ef43022d7256b086befc
khalid.zennouhi <144844220+KHALID-ZENNOUHI@users.noreply.github.com>
2024-09-22 15:09:51 +0200

    check the client the client name until get the client

--- src/BatiCuisine/Main.java
+++ src/BatiCuisine/Main.java
@@ -16,15 +16,19 @@ public class Main {
         while (true) {
             System.out.println("<--------BatiCuisine Project Management-------->");
             System.out.println("Choose one of the following options:");
+
             System.out.println("    -> 1. Create a new project");
-            System.out.println("    -> 2. Exit");
+            System.out.println("    -> 2. Display all projects");
+            System.out.println("    -> 3. Exit");
+
             System.out.print("Enter your choice: ");
             int choice = scanner.nextInt();
-            scanner.nextLine(); // Consume the newline
+            scanner.nextLine();
 
             switch (choice) {
                 case 1:
                     System.out.println("Would you like to search for an existing client or create a new one?");
+
                     System.out.println("    -> 1. Search for a client");
                     System.out.println("    -> 2. Create a new client");
                     int clientChoice;
@@ -36,19 +40,24 @@ public class Main {
                     } while (clientChoice != 1 && clientChoice != 2);
 
                     if (clientChoice == 1) {
-                        String clientName = clientManager.searchClient();
-                        if (clientName == null) {
-                            System.out.println("Client not found.");
-                        } else {
-                            projectManager.createProject(clientName); // Updated: projectManager will handle client ID
-                        }
+                        String clientName;
+                        do {
+                            clientName = clientManager.searchClient();
+                        }while (clientName == null);
+
+                        projectManager.createProject(clientName);
                     } else if (clientChoice == 2) {
                         String clientName = clientManager.createClient();
-                        projectManager.createProject(clientName); // Updated: projectManager will handle client ID
+                        projectManager.createProject(clientName);
                     }
                     break;
 
                 case 2:
+                    System.out.println("Displaying all projects...");
+                    projectManager.displayAllProjects();
+                    break;
+
+                case 3:
                     System.out.println("Exiting...");
                     System.exit(0);
                     break;
